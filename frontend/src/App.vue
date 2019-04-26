c25f35607bd77479c544b4bea1343834383c8c3e
infinity899 <pirvulescu.andrei91@yahoo.com>
2019-04-26 23:25:17 +0300

    import files + scss

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header/>
+    <img src="./assets/header.svg" alt="">
     <router-view>
     </router-view>
     <Footer/>
@@ -21,13 +22,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './scss/variables';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Helvetica', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $text-primary;
   margin-top: 60px;
 }
 </style>
